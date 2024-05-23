9e136503b415c1f745c91c457f2b1dd5970d53df
zerodevelpr_git <iman6978ranjbar@gmail.com>
2024-05-23 23:07:59 +0330

    🎉 Init

---
+++ src/main/java/net/zerodevelopr/artinshop/ArtinShopApplication.java
@@ -0,0 +1,13 @@
+package net.zerodevelopr.artinshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtinShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtinShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/zerodevelopr/artinshop/ArtinShopApplicationTests.java
@@ -0,0 +1,13 @@
+package net.zerodevelopr.artinshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtinShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
